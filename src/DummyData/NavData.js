59727e93571cdbbde44af623ce129e5864e1765c
mostlyclicks <carlos@mostlyclicks.com>
2021-11-03 20:05:40 -0600

    add DummyData dir and NavData

---
+++ src/DummyData/NavData.js
@@ -0,0 +1,12 @@
+export const topNavLinks = [
+  {url: "/example1", linkText: "Example One"},
+  {url: "/example2", linkText: "Example Two"},
+  {url: "/example3", linkText: "Example Three"}
+]
+
+export const mainNavLinks = [
+  {url: "/", linkText: "Home"},
+  {url: "/services", linkText: "Services"},
+  {url: "/about", linkText: "About"},
+  {url: "/contact", linkText: "Contact"},
+]
