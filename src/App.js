f937e4507ecb0ad893ec1eac0a872eaabc6f38d6
mostlyclicks <carlos@mostlyclicks.com>
2021-11-05 15:27:53 -0600

    start flex on cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Layout from './Layout/Layout'
 import TopNav from './Navs/TopNav'
+import Card from './Layout/Card'
 import Header from './Layout/Header'
 import Footer from './Layout/Footer'
+
 import { topNavLinks, mainNavLinks } from './DummyData/NavData'
 
 
@@ -16,6 +18,12 @@ function App({props}) {
           Add routing for pages
           - start with home
         */}
+        <div className="flex-auto flex-col">
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        
       <Footer primaryLinks={mainNavLinks} secondaryLinks={topNavLinks} />
     </Layout>
   );
--- src/Layout/Card.js
+++ src/Layout/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Card = () => {
   return (
-    <div>
+    <div style={{border: "1px solid gray"}}>
       <div>
         Header
       </div>
