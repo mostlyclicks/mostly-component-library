6f54ed72c86e5f3ff5866829dadac5613d9acf7e
mostlyclicks <carlos@mostlyclicks.com>
2021-11-06 19:56:16 -0600

    working with height

--- src/Layout/Hero.js
+++ src/Layout/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="h-96 bg-blue-400 w-screen -mt-8 flex flex-row justify-center mb-12">
+    <div className="bg-blue-400 w-screen -mt-8 justify-center mb-12 h-full">
       <h1 className='flex items-center'>Home Hero Section</h1>
     </div>
   )
