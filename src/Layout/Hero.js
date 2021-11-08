ce996ae9d4d6e67f30e816e22d52440a64aada21
mostlyclicks <carlos@mostlyclicks.com>
2021-11-07 21:16:44 -0700

    minheight in TW config

--- src/Layout/Hero.js
+++ src/Layout/Hero.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="bg-blue-400 w-screen -mt-8 justify-center mb-12 h-full">
+    <div className="bg-blue-400 w-screen -mt-8 flex flex-row justify-center mb-12 min-h-hero">
       <h1 className='flex items-center'>Home Hero Section</h1>
     </div>
   )
