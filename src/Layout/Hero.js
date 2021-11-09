038170404ac072a2ba06723f1ffaa33f23e8ec04
mostlyclicks <carlos@mostlyclicks.com>
2021-11-08 19:41:49 -0700

    add styling to main nav, position mainnav vs hero

--- src/Layout/Hero.js
+++ src/Layout/Hero.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import BikeImg from "../images/MY22_ADH01_Red.jpg"
 
 const Hero = () => {
   return (
-    <div className="bg-blue-hero bg-no-repeat bg-cover w-screen -mt-8 flex flex-row justify-center mb-12 min-h-hero"
-      
-    >
+    <div className="bg-blue-hero bg-no-repeat bg-cover w-screen -mt-16 flex flex-row justify-center mb-12 min-h-hero">
       <h1 className='flex items-center'>Home Hero Section</h1>
-
     </div>
   )
 } 
--- src/Navs/MainNav.js
+++ src/Navs/MainNav.js
@@ -6,12 +6,12 @@ const MainNav = (props) => {
   const links = props.links
 
   return (
-    <nav className="flex flex-row justify-between">
+    <nav className="flex flex-row justify-between pt-8">
       <Logo brand="Bike Site"/>
       <ul className="flex flex-row gap-6 items-center">
         {links.map((link) => {
           return (
-            <li><a href={link.url}>{link.linkText}</a></li>
+            <li><a href={link.url} className="text-white">{link.linkText}</a></li>
           )
         })}
       </ul>
