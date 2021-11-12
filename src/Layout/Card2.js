86704a03d43f3080ef425ff9a8577168c8d39bb0
mostlyclicks <carlos@mostlyclicks.com>
2021-11-11 19:17:01 -0700

    bigger card

--- src/Layout/Card2.js
+++ src/Layout/Card2.js
@@ -3,8 +3,8 @@ import BikeImg from "../images/MY22_ADH01_Red.jpg"
 
 const Card2 = () => {
   return (
-    <div className="md:w-1/2 flex flex-row bg-clip-content">
-      <img src={BikeImg} alt="Time Red bike" className=" w-1/2 self-baseline h-150" />
+    <div className=" flex flex-col bg-clip-content transition-opacity duration-500 ease-in">
+      <img src={BikeImg} alt="Time Red bike" className="  self-center h-150" />
       <div className="p-4">
         <h1>TIME ALPE D'HUEZ 01</h1>
         <p className="hidden md:flex">Time Bicycles is well known around the world for having some of the most comfortable and performative rides available. The Alpe d'Huez 01 Rim Brake Frameset is proof positive of the luxury brand's commitment to quality and performance.</p>
