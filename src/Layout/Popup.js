86335dd3d5c15a8156ace4e3e80fcfd8ef97e8df
mostlyclicks <carlos@mostlyclicks.com>
2021-11-11 19:48:50 -0700

    add and position modal

--- src/Layout/Popup.js
+++ src/Layout/Popup.js
@@ -3,9 +3,13 @@ import React from 'react'
 const Popup = () => {
   return (
     <div>
-      <div className="bg-black opacity-80 w-screen h-screen z-100 absolute">
-        adfsd
+      <div className="flex justify-center absolute z-20 w-screen h-screen">
+        <div className="flex bg-white w-1/3 h-1/3 z-200 relative rounded-md self-center">Hello</div>  
       </div>
+      <div className="bg-black opacity-80 w-screen h-screen z-10 absolute ">
+        
+      </div>
+      
 
     </div>
   )
