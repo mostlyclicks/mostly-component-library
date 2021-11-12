0cdbbe7d99a5e1baa67c73e079be016f3f26d9a4
mostlyclicks <carlos@mostlyclicks.com>
2021-11-11 21:02:25 -0700

    add close modal on invisible div

--- src/Layout/Overlay.js
+++ src/Layout/Overlay.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 
 const Overlay = () => {
+
   return (
     <Fragment>
       <div className="bg-black opacity-80 w-screen h-screen z-10 absolute" />
--- src/Layout/Popup.js
+++ src/Layout/Popup.js
@@ -3,9 +3,13 @@ import Overlay from './Overlay'
 
 const Popup = (props) => {
 
+//  const handleCloseModal = () => {
+//    props.
+//  }
+
   return (
     <div>
-      <div className="flex justify-center absolute z-20 w-screen h-screen">
+      <div className="flex justify-center absolute z-20 w-screen h-screen" onClick={props.closeModal}>
         <div className="flex bg-white w-1/3 h-1/3 z-200 relative rounded-md self-center">
           <p>Hello, this is a modal.</p>
           <button onClick={props.closeModal}>Close modal</button>
