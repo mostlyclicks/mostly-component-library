773cde628576adf582a1e2f0c933257dc9c05a89
mostlyclicks <carlos@mostlyclicks.com>
2021-11-14 20:58:04 -0700

    tweak mobile nav

--- src/Layout/Popup.js
+++ src/Layout/Popup.js
@@ -6,7 +6,7 @@ const Popup = (props) => {
   return (
     <div>
       <div className="flex justify-center absolute z-20 w-screen h-screen" onClick={props.closeModal}>
-        <div className="flex bg-white w-1/3 h-1/3 z-200 relative rounded-md self-center">
+        <div className="flex bg-white w-4/5 sm:w-1/2 md:w-1/3 h-1/3 z-200 relative rounded-md self-center">
           <p>Hello, this is a modal.</p>
           <button onClick={props.closeModal}>Close modal</button>
         </div>  
--- src/Navs/MobileNav.js
+++ src/Navs/MobileNav.js
@@ -6,8 +6,8 @@ const MobileNav = (props) => {
   const links = props.links
 
   return (
-    <div className="z-300 fixed top-8 left-0 flex flex-col w-full h-96 bg-black opacity-90">
-      <button onClick={props.onCloseNav} className="text-white text-4xl self-end pt-0 pr-6">–</button>
+    <div className="z-300 fixed top-8 left-0 flex flex-col w-full h-screen bg-black opacity-90">
+      <button onClick={props.onCloseNav} className="text-white text-4xl self-end -mt-2 pr-5">–</button>
       <ul className="m-0 p-8">
         {links.map((link) => {
 
