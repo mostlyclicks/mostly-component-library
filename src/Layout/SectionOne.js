4be09d3418c8382e4eb82ed7faaf59d7f4f3f4fd
mostlyclicks <carlos@mostlyclicks.com>
2021-12-07 21:33:58 -0700

    get pict overlapping div

--- src/Layout/SectionOne.js
+++ src/Layout/SectionOne.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import BikeImg from "../images/MY22_ADH01_Red.jpg"
 
 const SectionOne = () => {
   return (
-    <div className="h-96 bg-gradient-to-r from-gray-700 via-white to-white border-2 border-red-500">
-      <h1>Section One</h1>
+    <div className="flex flex-row">
+      <div className="h-96 w-2/3 bg-gradient-to-r from-gray-700 via-white to-white border-2 border-red-500">
+        <h1>Section One</h1>
 
+      </div>
+      <img src={BikeImg} alt="bike" className="w-1/3 h-1/2 self-center -ml-24" />
+      
     </div>
+    
   )
 }
 
